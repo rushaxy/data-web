4aa15d70ffd477ac7789edc0e1fa4c237f3de084
helong <helong@starcdata.com>
2022-11-18 17:07:48 +0800

    修改地址，方便本地测试

---
+++ datax-executor/src/main/java/com/wugui/datax/executor/DataXExecutorApplication1.java
@@ -0,0 +1,16 @@
+package com.wugui.datax.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuxueli 2018-10-28 00:38:13
+ */
+@SpringBootApplication
+public class DataXExecutorApplication1 {
+
+	public static void main(String[] args) {
+        SpringApplication.run(DataXExecutorApplication1.class, args);
+	}
+
+}
